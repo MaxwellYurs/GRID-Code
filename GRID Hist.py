5da5cb36c967106a748a7edce1b966e9bbbe738c
MaxwellYurs <yursx002@umn.edu>
2016-06-15 12:33:45 -0500

    Added for loops
    You know what happened at this point.

--- GRID Hist.py
+++ GRID Hist.py
@@ -1,17 +1,39 @@
 # coding=utf-8
 import matplotlib.pyplot as plt #imports the matplot library pyplot and names it plt
 
-Channel1=[2.1,2.3,2.2,2,3,2.25,2.6,5,5,6,6,7,7,7,7,7]  #Channel 1 Data (need to figure out how to import data array
-Channel2=[3,3,3,4,1,4,6,6,6,6,3,3,3,9,9,9,9,9,7]  #Channel 2 Data  and subarray by channel)
-Channel3=[1,1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4,4,4]  #Channel 3 Data
-Channel4=[2,2,2,2,2,2,2,2,2,2,2,7,7,7,7,7,7,7,7]  #Channel 4 Data
+import csv
+
+Channel1 = []                         #initializing the lists for each channel to be filled from file
+Channel2 = []
+Channel3 = []
+Channel4 = []
+TotalCounts = []
+                                                                            #opening the .csv file from the serial port. File needs to be saved
+with open('firstCoolTermUse1.csv') as csvfile:                              #in the same folder as the code being written. Must select file before
+    readCSV = csv.reader(csvfile, delimiter=',')                            #running 
+    for row in readCSV:
+        TotalCounts.append(float(row[2]))
+        if float(row[0]) == 1:
+            Channel1.append(float(row[2]))
+
+        if float(row[0]) == 2:
+            Channel2.append(float(row[2]))
+
+        if float(row[0]) == 3:
+            Channel3.append(float(row[2]))
+
+        if float(row[0]) == 4:
+            Channel4.append(float(row[2]))
+
+
 
 Bins1=3    #<-----Channel 1 number of Bins (Can be modified)
 Bins2=25    #<-----Channel 2 number of Bins (Can be modified)
 Bins3=25    #<-----Channel 3 number of Bins (Can be modified)
 Bins4=25    #<-----Channel 4 number of Bins (Can be modified)
+BinsTC=25    #<-----Channel 4 number of Bins (Can be modified)
 
-fig, axes = plt.subplots(1,4, figsize=(15,3)) #<----defines the number of plots, figure size(width,height) in inches
+fig, axes = plt.subplots(1,5, figsize=(15,3)) #<----defines the number of plots, figure size(width,height) in inches
 
 axes[0].hist(Channel1,bins=Bins1,histtype='step'
 ,range=(2,3),normed=False)                                   #Directions for adjusting Plot attributes:
@@ -37,6 +59,14 @@ axes[3].hist(Channel4,bins=Bins4,histtype='step'
 axes[3].set_title("Channel 4")
 axes[3].set_xlabel("volts")
 axes[3].set_ylabel("counts")
+
+axes[4].hist(TotalCounts,bins=BinsTC,histtype='step'
+,range=None,normed=False)
+axes[4].set_title("Total Counts")
+axes[4].set_xlabel("volts")
+axes[4].set_ylabel("counts")
+
 plt.show()
+
 
 #fig.tight_layout()  #<------automatically aligns the plots so they don't inadvertantly over lap
